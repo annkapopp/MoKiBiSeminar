8dca6c9132fa81d95a0f5c6fd6f9eaeb9fb9dc65
annkapopp <88618677+annkapopp@users.noreply.github.com>
2023-09-11 16:27:48 +0200

    renamed train file and added more augmentation

--- dataset.py
+++ dataset.py
@@ -9,19 +9,30 @@ import numpy as np
 import pandas as pd
 
 
-LABELS = ["Atelectasis", "Consolidation", "Pneumothorax", "Edema", "Effusion", "Pneumonia", "Cardiomegaly",
+LABELS_USED = ["Atelectasis", "Consolidation", "Pneumothorax", "Edema", "Effusion", "Pneumonia", "Cardiomegaly",
           "No Finding"]
+
+ALL_LABELS = ['Atelectasis', 'Consolidation', 'Infiltration', 'Pneumothorax', 'Edema', 'Emphysema', 'Fibrosis',
+              'Effusion', 'Pneumonia', 'Pleural_Thickening', 'Cardiomegaly', 'Nodule', 'Mass', 'Hernia',
+              'Lung Lesion', 'Fracture', 'Lung Opacity', 'Enlarged Cardiomediastinum']
 
 
 def augmentation(image):
     image = TF.resize(image, [256, 256], antialias=True)
     image = transforms.RandomHorizontalFlip(0.5)(image)
-    image = TF.equalize(image)
+    image = TF.affine(image.unsqueeze(0),
+                      translate=[np.random.rand()/10*image.shape[0], np.random.rand()/10*image.shape[1]],
+                      angle=np.random.randint(-20, 20),
+                      shear=[np.random.rand()/10*image.shape[0], np.random.rand()/10*image.shape[1]],
+                      scale=1.0)
     image = TF.adjust_gamma(image, np.random.rand() + 0.5)
-    angle = np.random.randint(-20, 20)
-    image = TF.rotate(image, angle, TF.InterpolationMode.BILINEAR, expand=False)
 
     return image
+
+
+def normalise(image):
+    # return ((image - torch.min(image)) / (torch.max(image) - torch.min(image)) * 2024) - 1024
+    return ((image - 0) / (255 - 0) * 2024) - 1024
 
 
 class NIHChestXRayDataset(Dataset):
@@ -34,19 +45,20 @@ class NIHChestXRayDataset(Dataset):
     def __getitem__(self, index):
         image_data = self.data.iloc[index]
         path = os.path.join(self.path_to_dir, "images", image_data["Image Index"])
-        # print(path)
         image = torch.from_numpy(cv2.imread(path, cv2.IMREAD_GRAYSCALE)).unsqueeze(0)
 
         if self.mode == "train":
             # augmentation
             image = augmentation(image)
+        else:
+            image = TF.resize(image, [224, 224], antialias=True)
 
-        image = (image - torch.min(image)) / (torch.max(image) - torch.min(image))
+        image = normalise(image)
 
         image_name = os.path.basename(path)
         one_hot = torch.from_numpy(np.asarray(self.data[self.data['Image Index'].str.contains(image_name)]
-                                              ['target_vector'].values[0]).astype(int))
-        label = LABELS[np.argmax(one_hot)]
+                                              ['one hot'].values[0]).astype(int))
+        label = LABELS_USED[np.argmax(one_hot)]
 
         return image, label, one_hot, image_name
 
@@ -54,7 +66,7 @@ class NIHChestXRayDataset(Dataset):
         return len(self.data)
 
     def __label_counts__(self):
-        label_counts = np.stack(self.data['one_hot'].values).sum(axis=0).astype(float)
+        label_counts = np.stack(self.data['one hot'].values).sum(axis=0).astype(float)
         return torch.from_numpy(label_counts)
 
 
@@ -78,7 +90,7 @@ class CheXpertDataset(Dataset):
         image_name = os.path.basename(path)
         one_hot = torch.from_numpy(np.asarray(self.data[self.data['Path'].str.contains(image_name)]
                                               ['target_vector'].values[0]).astype(int))
-        label = LABELS[np.argmax(one_hot)]
+        label = LABELS_USED[np.argmax(one_hot)]
 
         return image, label, one_hot, image_name
 
@@ -92,17 +104,13 @@ class CheXpertDataset(Dataset):
 
 
 if __name__ == "__main__":
-    dataset_train = NIHChestXRayDataset(path_to_dir="E:\\NIH Chest Xray", mode="train")
+    dataset_train = NIHChestXRayDataset(path_to_dir="NIH Chest Xray", mode="train")
+    dataset_val= NIHChestXRayDataset(path_to_dir="NIH Chest Xray", mode="val")
+    dataset_test = NIHChestXRayDataset(path_to_dir="NIH Chest Xray", mode="test")
     #dataset_test = NIHChestXRayDataset(path_to_dir="E:\\NIH Chest Xray", mode="test")
     #image = dataset_train.__getitem__(0)
-    import multiprocessing as mp
-    from torch.utils.data import DataLoader
-    from time import time
-    for num_workers in range(4, mp.cpu_count(), 2):
-        train_loader = DataLoader(dataset_train, shuffle=True, num_workers=num_workers, batch_size=24, pin_memory=True)
-        start = time()
-        for i, data in enumerate(train_loader, 0):
-            pass
-        end = time()
-        print("Finish with:{} second, num_workers={}".format(end - start, num_workers))
+
+    print(dataset_train.__label_counts__(), dataset_train.__label_counts__() / dataset_train.__len__())
+    print(dataset_val.__label_counts__(), dataset_val.__label_counts__() / dataset_val.__len__())
+    print(dataset_test.__label_counts__(), dataset_test.__label_counts__() / dataset_test.__len__())
 
--- train_base_model.py
+++ train_base_model.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 import pickle as pkl
 
-from resnet import ResNet18
+from base_models import ResNet18, DenseNet121
 import dataset
 from evaluation_metrics import accuracy
 import utils
@@ -29,20 +29,14 @@ def train(model, model_name, num_epochs, train_dataloader, val_dataloader, crite
 
         model.train()
 
-        # start_load = time.time()
         for input, _, target, _ in train_dataloader:
-            # end_load = time.time()
-            # print("load time: ", end_load - start_load)
-
             input = input.to(DEVICE)
             target = target.to(DEVICE)
             optimizer.zero_grad()
 
-            # start_model = time.time()
-
             output = model(input)
             class_prediction = utils.get_class_prediction(output)
-            output, class_prediction = utils.constrain_prediction(output.clone(), class_prediction)
+            #output, class_prediction = utils.constrain_prediction(output.clone(), class_prediction)
 
             loss = criterion(output, target.float())
             loss.backward()
@@ -53,16 +47,11 @@ def train(model, model_name, num_epochs, train_dataloader, val_dataloader, crite
             running_accuracy += mean_accuracy.detach().cpu().numpy()
             optimizer.step()
 
-            # start_load = time.time()
-
         running_loss /= (len(train_dataloader) * dataloader_train.batch_size)
         running_accuracy /= len(train_dataloader)
-        # f1 = f1_score(y_true=target.detach().cpu().numpy(), y_pred=output.detach().cpu().numpy(),
-                      # average='micro')
 
         log["loss_train"].append(running_loss)
         log["accuracy_train"].append(running_accuracy)
-        # log["f1_train"].append(f1)
 
         # output
         #if epoch % 5 == 0:
@@ -82,7 +71,7 @@ def train(model, model_name, num_epochs, train_dataloader, val_dataloader, crite
                 output = model(input)
 
                 class_prediction = utils.get_class_prediction(output)
-                output, class_prediction = utils.constrain_prediction(output, class_prediction)
+                #output, class_prediction = utils.constrain_prediction(output, class_prediction)
 
                 loss = criterion(output, target.float())
 
@@ -93,46 +82,45 @@ def train(model, model_name, num_epochs, train_dataloader, val_dataloader, crite
 
             running_loss /= (len(val_dataloader) * dataloader_val.batch_size)
             running_accuracy /= len(val_dataloader)
-            # f1 = f1_score(y_true=target.detach().cpu().numpy(), y_pred=output.detach().cpu().numpy(),
-                          # average='micro')
 
             scheduler.step()
 
             log["loss_val"].append(running_loss)
             log["accuracy_val"].append(running_accuracy)
-            # log["f1_val"].append(f1)
 
             # output
-            torch.save(model.state_dict(), "E:\\trained_models\\" + model_name + ".pt")
-            with open("E:\\trained_models\\" + model_name + "_train_log.pkl", "wb") as f:
+            torch.save(model.state_dict(), "trained_models/" + model_name + ".pt")
+            with open("trained_models/" + model_name + "_train_log.pkl", "wb") as f:
                 pkl.dump(log, f)
 
             tqdm.write('Epoch {} (valid) -- loss: {:.4f} acc: {:.4f}'.format(epoch, running_loss,
                                                                              running_accuracy))
 
-    torch.save(model.state_dict(), "E:\\trained_models\\" + model_name + ".pt")
-    with open("E:\\trained_models\\" + model_name + "_train_log.pkl", "wb") as f:
+    torch.save(model.state_dict(), "trained_models/" + model_name + ".pt")
+    with open("trained_models/" + model_name + "_train_log.pkl", "wb") as f:
         pkl.dump(log, f)
 
 
 if __name__ == "__main__":
     print("device: ", DEVICE)
 
-    dataset_train = dataset.NIHChestXRayDataset("E:\\NIH Chest Xray", mode="train")
-    dataset_val = dataset.NIHChestXRayDataset("E:\\NIH Chest Xray", mode="val")
+    dataset_train = dataset.NIHChestXRayDataset("NIH Chest Xray", mode="train")
+    dataset_val = dataset.NIHChestXRayDataset("NIH Chest Xray", mode="val")
 
-    dataloader_train = DataLoader(dataset_train, batch_size=24, shuffle=True, pin_memory=True, num_workers=8)
-    dataloader_val = DataLoader(dataset_val, batch_size=24, shuffle=False, pin_memory=True, num_workers=8)
+    dataloader_train = DataLoader(dataset_train, batch_size=32, shuffle=True, pin_memory=True, num_workers=8)
+    dataloader_val = DataLoader(dataset_val, batch_size=32, shuffle=False, pin_memory=True, num_workers=8)
 
     label_counts = dataset_train.__label_counts__()
     weights = np.sqrt(1 / label_counts)
     weights /= weights.mean()
 
-    model = ResNet18().to(DEVICE)
+    # model = ResNet18(pretrained=False).to(DEVICE)
+    model = DenseNet121(pretrained=True).to(DEVICE)
     lr = 0.00001
     criterion = nn.BCELoss(weight=weights.to(DEVICE, dtype=torch.float))
+    # criterion = nn.L1Loss()
     optimizer = optim.Adam(model.parameters(), lr)
     scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
 
-    train(model, "nih_chest_resnet18", 35, dataloader_train, dataloader_val,
+    train(model, "nih_chest_densenet_pretrained_new", 10, dataloader_train, dataloader_val,
           criterion, optimizer, scheduler)
